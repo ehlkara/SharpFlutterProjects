c5781d233ec54209f2e2980027d493885e930a3e
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-25 15:19:00 +0300

    displaying images

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -6,10 +6,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ),
+        body: GradientContainer.purple(),
       ),
     ),
   );
