e628bd2225440abc7b065e0c77dfba48a9b8de8e
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-26 10:55:38 +0300

    created questionsScreen and quiz dart

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -35,7 +35,9 @@ class StartScreen extends StatelessWidget {
             height: 30,
           ),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              // ...
+            },
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
