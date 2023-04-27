c09e38b8080551ec4a31d26fe34f989122f94562
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-27 14:20:13 +0300

    parsing data to the results Screen

--- adv_basics/lib/results_screen.dart
+++ adv_basics/lib/results_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.choosenAnswers});
+
+  final List<String> choosenAnswers;
 
   @override
   Widget build(BuildContext context) {
