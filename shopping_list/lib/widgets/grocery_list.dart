1d8ecbaec59a805730d0f8ccf65ba3b42757d5e5
ehlkara <ehlullah.fb@gmail.com>
2023-05-16 14:19:51 +0300

    avoding unnecessary requests

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -27,13 +27,13 @@ class _GroceryListState extends State<GroceryList> {
         'flutter-shop-77470-default-rtdb.firebaseio.com', 'shopping-list.json');
     final response = await http.get(url);
     final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
+    final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
       final category = categories.entries
           .firstWhere(
               (catItem) => catItem.value.title == item.value['category'])
           .value;
-      _loadedItems.add(
+      loadedItems.add(
         GroceryItem(
           id: item.key,
           name: item.value['name'],
@@ -42,16 +42,22 @@ class _GroceryListState extends State<GroceryList> {
         ),
       );
     }
-    _groceryItems = _loadedItems;
+    _groceryItems = loadedItems;
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
-    _loadItems();
+
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   void _removeItem(GroceryItem item) {
