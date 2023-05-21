59cef92f40fedc021a2919c0f74cdca128a9bb29
ehlkara <ehlullah.fb@gmail.com>
2023-05-21 15:55:32 +0300

    using picked location in the form

--- favorite_place/lib/screens/add_place.dart
+++ favorite_place/lib/screens/add_place.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:favorite_place/models/place.dart';
 import 'package:favorite_place/providers/user_places.dart';
 import 'package:favorite_place/widgets/image_input.dart';
 import 'package:favorite_place/widgets/location_input.dart';
@@ -18,6 +19,7 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
@@ -26,9 +28,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
       return;
     }
 
-    ref
-        .read(userPlacesProvider.notifier)
-        .addPlace(enteredTitle, _selectedImage!);
+    ref.read(userPlacesProvider.notifier).addPlace(
+          enteredTitle,
+          _selectedImage!,
+          _selectedLocation!,
+        );
 
     Navigator.of(context).pop();
   }
@@ -63,7 +67,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               },
             ),
             const SizedBox(height: 10),
-            LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
