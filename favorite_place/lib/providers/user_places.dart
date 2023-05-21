c0bc31a3fcd7c9fa5c973ffc70e68a71b1c09dd9
ehlkara <ehlullah.fb@gmail.com>
2023-05-21 11:28:07 +0300

    adding riverpod package

---
+++ favorite_place/lib/providers/user_places.dart
@@ -0,0 +1,15 @@
+import 'package:favorite_place/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacessNotifier extends StateNotifier<List<Place>> {
+  UserPlacessNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider(
+  (ref) => UserPlacessNotifier(),
+);
